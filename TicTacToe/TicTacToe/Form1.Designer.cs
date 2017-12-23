3921eb88bc581f0859471a0bf2234261ad3446d2
SEJean <jeank714@gmail.com>
2017-12-22 22:24:47 -0500

    Added a game status in HUD

--- TicTacToe/TicTacToe/Form1.Designer.cs
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.pnlGrid = new System.Windows.Forms.Panel();
             this.grpScore = new System.Windows.Forms.GroupBox();
-            this.btnReplay = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblO = new System.Windows.Forms.Label();
-            this.lblXScore = new System.Windows.Forms.Label();
             this.lblOScore = new System.Windows.Forms.Label();
+            this.lblXScore = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReplay = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblGameStatus = new System.Windows.Forms.Label();
             this.grpScore.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,34 +61,15 @@
             this.grpScore.TabStop = false;
             this.grpScore.Text = "Score";
             // 
-            // btnReplay
+            // lblOScore
             // 
-            this.btnReplay.Location = new System.Drawing.Point(288, 223);
-            this.btnReplay.Name = "btnReplay";
-            this.btnReplay.Size = new System.Drawing.Size(200, 54);
-            this.btnReplay.TabIndex = 2;
-            this.btnReplay.Text = "Replay";
-            this.btnReplay.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label1.Location = new System.Drawing.Point(6, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "X";
-            // 
-            // lblO
-            // 
-            this.lblO.AutoSize = true;
-            this.lblO.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.lblO.Location = new System.Drawing.Point(6, 68);
-            this.lblO.Name = "lblO";
-            this.lblO.Size = new System.Drawing.Size(43, 39);
-            this.lblO.TabIndex = 1;
-            this.lblO.Text = "O";
+            this.lblOScore.AutoSize = true;
+            this.lblOScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblOScore.Location = new System.Drawing.Point(109, 68);
+            this.lblOScore.Name = "lblOScore";
+            this.lblOScore.Size = new System.Drawing.Size(36, 39);
+            this.lblOScore.TabIndex = 3;
+            this.lblOScore.Text = "0";
             // 
             // lblXScore
             // 
@@ -98,21 +81,61 @@
             this.lblXScore.TabIndex = 2;
             this.lblXScore.Text = "0";
             // 
-            // lblOScore
+            // lblO
             // 
-            this.lblOScore.AutoSize = true;
-            this.lblOScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.lblOScore.Location = new System.Drawing.Point(109, 68);
-            this.lblOScore.Name = "lblOScore";
-            this.lblOScore.Size = new System.Drawing.Size(36, 39);
-            this.lblOScore.TabIndex = 3;
-            this.lblOScore.Text = "0";
+            this.lblO.AutoSize = true;
+            this.lblO.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblO.Location = new System.Drawing.Point(6, 68);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(43, 39);
+            this.lblO.TabIndex = 1;
+            this.lblO.Text = "O";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // btnReplay
+            // 
+            this.btnReplay.Location = new System.Drawing.Point(288, 223);
+            this.btnReplay.Name = "btnReplay";
+            this.btnReplay.Size = new System.Drawing.Size(200, 54);
+            this.btnReplay.TabIndex = 2;
+            this.btnReplay.Text = "Replay";
+            this.btnReplay.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblStatus.Location = new System.Drawing.Point(13, 298);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(108, 37);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblGameStatus
+            // 
+            this.lblGameStatus.AutoSize = true;
+            this.lblGameStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblGameStatus.Location = new System.Drawing.Point(143, 298);
+            this.lblGameStatus.Name = "lblGameStatus";
+            this.lblGameStatus.Size = new System.Drawing.Size(0, 37);
+            this.lblGameStatus.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 299);
+            this.ClientSize = new System.Drawing.Size(502, 357);
+            this.Controls.Add(this.lblGameStatus);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnReplay);
             this.Controls.Add(this.grpScore);
             this.Controls.Add(this.pnlGrid);
@@ -124,6 +147,7 @@
             this.grpScore.ResumeLayout(false);
             this.grpScore.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -136,6 +160,8 @@
         private System.Windows.Forms.Label lblXScore;
         private System.Windows.Forms.Label lblO;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblGameStatus;
     }
 }
 
