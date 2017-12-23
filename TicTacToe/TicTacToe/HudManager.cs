f44ff23849b66ce8bde73e531ecd4ef9b47ca958
SEJean <jeank714@gmail.com>
2017-12-22 23:44:41 -0500

    Added ReadMe

---
+++ TicTacToe/TicTacToe/HudManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+
+
+    public class HudManager
+    { 
+        public Label GameStatus { get; set; }
+        public Label XScore { get; set; }
+        public Label OScore { get; set; }
+
+        public void SetStatus(string p_Message)
+        {
+            GameStatus.Text = p_Message;
+        }
+
+        public void SetScore(char p_Player)
+        {
+            int score;
+            Label lblPlayer;
+            if (p_Player == 'X')
+            {
+                score = Convert.ToInt32(XScore.Text);
+                lblPlayer = XScore;
+            }
+            else
+            {
+                score = Convert.ToInt32(OScore.Text);
+                lblPlayer = OScore;
+            }
+            score++;
+            lblPlayer.Text = score.ToString();
+        }
+    }
+}
